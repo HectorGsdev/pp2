bfa540daab2248054a07865084435ff376a963f4
hector <gonzalez.hector@student.greenriver.edu>
2022-01-16 18:36:06 -0500

    Step 1 of PP2 asigment

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+// Name Hector Gonzalez
+// Date 01-16-22
+// URL:
+// Pair Programing 2
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Programing 2</title>
+</head>
+<body>
+<h1>Pair Programing 2</h1>
+<?php
+echo ("Step 1 " ."<br>");
+echo("PHP array practice");
+
+?>
+
+</body>
+</html>
