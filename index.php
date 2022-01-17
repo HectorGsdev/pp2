82bf8f2ba6829b82b5f5234c4ba80f6f0f0c28e3
hector <gonzalez.hector@student.greenriver.edu>
2022-01-16 19:01:02 -0500

    Step 3 of the PP2 assigment

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 // Name Hector Gonzalez
 // Date 01-16-22
-// URL:
+// URL: https://github.com/HectorGsdev/pp2.git
 // Pair Programing 2
 
 
@@ -18,7 +18,18 @@ error_reporting(E_ALL);
 <h1>Pair Programing 2</h1>
 <?php
 echo ("Step 1 " ."<br>");
-echo("PHP array practice");
+echo("PHP array practice"."<br>");
+$numbers=array(7, 9, 8, 9, 8, 8, 6);
+printArr($numbers);
+
+function printArr($numbers) {
+    for ($x = 0; $x <= sizeof($numbers)-1; $x++) {
+        echo "element: $x of the array is $numbers[$x] <br>";
+    }
+}
+
+
+
 
 ?>
 
